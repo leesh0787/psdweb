f446e75ae99edd6d5459003f4f09c074f12c82e8
leesh0787 <131154479+leesh0787@users.noreply.github.com>
2023-10-30 15:11:40 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+  $('.banner_list').slick({
+    dots:true,autoplay:true,
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* $(document).ready(function(){
+  $('.main_content .project_list li').hover(
+    function(){
+      $(this).show('.main_content .project_list .hover_contents')
+    }
+  )
+
+})
+ */
